7bbbc20706130140ae4d15a63c8e1fd78e61b2db
weerotheme <weerotheme1@gmail.com>
2023-08-17 23:53:30 +0600

    slider add

--- js/main.js
+++ js/main.js
@@ -19,39 +19,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // =========slider ===========;
 
-document.addEventListener("DOMContentLoaded", function () {
-  const items = document.querySelectorAll(".slider-container .item");
-  const dotLength = document.querySelector(".dot-length");
-  const totalItems = items.length;
-  let currentIndex = 0;
-
-  function updateDotLengthWidth() {
-    const width = (currentIndex / (totalItems - 1)) * 100;
-    dotLength.style.width = width + "%";
-  }
-
-  function showSlide(index) {
-    items[currentIndex].classList.remove("active");
-    items[index].classList.add("active");
-    currentIndex = index;
-    updateDotLengthWidth();
-  }
-
-  function showNextSlide() {
-    const nextIndex = (currentIndex + 1) % totalItems;
-    showSlide(nextIndex);
-  }
-
-  const sliderInterval = setInterval(showNextSlide, 5000);
-
-  const sliderContainer = document.querySelector(".slider-container");
-  sliderContainer.addEventListener("mouseenter", function () {
-    clearInterval(sliderInterval);
-  });
-
-  sliderContainer.addEventListener("mouseleave", function () {
-    sliderInterval = setInterval(showNextSlide, 5000);
-  });
-
-  showSlide(currentIndex);
+$(".regular").slick({
+  slidesToShow: 2,
+  arrows: false,
+  centerMode: true,
+  centerPadding: "40px",
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
 });
